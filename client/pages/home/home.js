90311fd3b026ac45b6c5e3f2d3008467dcdd07e3
Evie Ma <yfma89@gmail.com>
2018-06-07 16:47:50 +1000

    use API to get product data

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,48 +1,48 @@
 // pages/home/home.js
+const config = require('../../config.js')
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    productList: [{
-      id: 1,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
-      name: '商品1',
-      price: 100,
-      source: '国内·广东',
-    }, {
-      id: 2,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
-      name: '商品2',
-      price: 200,
-      source: '国内·广东',
-    }, {
-      id: 3,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
-      name: '商品3',
-      price: 300,
-      source: '国内·广东',
-    }, {
-      id: 4,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
-      name: '商品4',
-      price: 400,
-      source: '国内·广东',
-    }, {
-      id: 5,
-      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product5.jpg',
-      name: '商品5',
-      price: 500,
-      source: '国内·广东',
-    }], // 商品列表
+    productList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getProductList()
+  },
+
+  getProductList(){
+    wx.showLoading({
+      title: '商品数据加载中...',
+    })
+    qcloud.request({
+      url: config.service.productList,
+      success: res => {
+        wx.hideLoading()
+        if (!res.data.code) {
+          this.setData({
+            productList: res.data.data
+          })
+        }
+        else {
+          wx.showToast({
+            title: '商品数据加载失败',
+          })
+        }
+      },
+      error: res => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '商品数据加载失败',
+        })
+      }
+    })
   },
 
   /**
