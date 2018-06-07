18604d97fbd0f8ecc86d7ab858e5cb6c68784162
Evie Ma <yfma89@gmail.com>
2018-06-07 18:24:14 +1000

    add product detail page, accessible on tap of product on home page

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,5 +2,15 @@ const DB = require('../utils/db.js')
 module.exports = {
   list: async ctx => {
     ctx.state.data = await DB.query("SELECT * FROM product;")
+  },
+  detail: async ctx => {
+    productID = + ctx.params.id // “+”强制把字符变为整数
+    
+    if (!isNaN(productID)) {
+      ctx.state.data = (await DB.query("SELECT * FROM product WHERE product.id = ?", [productID]))[0]
+    }
+    else {
+      ctx.state.data = {}
+    }
   }
 }
