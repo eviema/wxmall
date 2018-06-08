0709cc7bb9325d044cde86712f57db9b239e8d3f
Evie Ma <yfma89@gmail.com>
2018-06-08 20:16:10 +1000

    adjust product count under edit mode on trolley page

--- client/pages/trolley/trolley.js
+++ client/pages/trolley/trolley.js
@@ -133,8 +133,50 @@ Page({
       isTrolleyEdit: !isTrolleyEdit
     })
   },
+
+  adjustTrolleyProductCount(event) {
+    let trolleyCheckMap = this.data.trolleyCheckMap
+    let trolleyList = this.data.trolleyList
+    let dataset = event.currentTarget.dataset
+    let adjustType = dataset.type
+    let productId = dataset.id
+    let product
+    let index 
+
+    for (index = 0; index < trolleyList.length; index++) {
+      if (productId === trolleyList[index].id) {
+        product = trolleyList[index]
+        break
+      }
+    }
+
+    if (product) {
+      if (adjustType === 'add') {
+        // 点击加号
+        product.count++
+      }
+      else if (adjustType === 'minus'){
+        // 点击减号
+        if (product.count <= 1) {
+          // 商品数量不超过1，点击减号相当于删除
+          delete trolleyCheckMap[productId]
+          trolleyList.splice(index, 1)
+        } 
+        else{
+          // 商品数量大于1
+          product.count--
+        }
+      }
+    }
+
+    let trolleyAccount = this.calcAccount(trolleyList, trolleyCheckMap)
+
+    this.setData({
+      trolleyAccount,
+      trolleyList,
       trolleyCheckMap
     })
+
   },
 
   /**
